b77207897713429c394adfd7716e0f692d2fb33d
Alyvia Biswas <alyvia02510@gmail.com>
2024-12-09 17:11:39 +0000

    Strings and Methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,31 @@
+const name = "Alyvia"
+const repoCount = 5
+
+console.log(name+repoCount+" Value")
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`)
+
+const gameName = new String('alyvia-ab')
+
+console.log(gameName[0])
+console.log(gameName.__proto__)
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('y'))
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString)
+
+const newStringOne = "    alyvia    "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "https://alyvia%20biswas.com"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('biswas'))
+console.log(url.includes('sun'))
+
+console.log(gameName.split('-'))
