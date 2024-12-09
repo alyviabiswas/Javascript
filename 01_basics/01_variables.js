10fdee115483e9cab1dcc43e8007490495378fb5
Alyvia Biswas <alyvia02510@gmail.com>
2024-12-09 06:35:20 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail= "alyviabiswas@google.com"
+var accountPwd= "4567"
+accountCity= "Kolkata"
+let accountState;
+// accountId= 4
+
+accountEmail= "alyvia@google.com"
+accountPwd= "2345"
+accountCity= "Chennai"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPwd,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all JS code as newer version
+console.log(2+5)
+console.log("Alyvia")
+
+let name= "Alyvia"
+let age= 21
+let isLoggedIn= false
+
+console.log(typeof "Alyvia")
+console.log(typeof age)
+console.log(typeof 18)
+console.log(typeof null)
+console.log(typeof undefined)
