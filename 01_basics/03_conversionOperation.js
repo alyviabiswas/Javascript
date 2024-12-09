a9e574dd41120a2bb90da54739f09506797121ce
Alyvia Biswas <alyvia02510@gmail.com>
2024-12-09 06:57:26 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "Alyvia"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanisLoggedIn)
+
+let randomNumber = 77
+let stringrandomNumber = String(randomNumber)
+console.log(stringrandomNumber)
+console.log(typeof stringrandomNumber)
