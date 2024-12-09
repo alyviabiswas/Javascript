7b601bede3532db3e1109ef735ac1da29f60f59f
Alyvia Biswas <alyvia02510@gmail.com>
2024-12-09 11:47:47 +0000

    conversions and comparisions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,3 +15,41 @@ let randomNumber = 77
 let stringrandomNumber = String(randomNumber)
 console.log(stringrandomNumber)
 console.log(typeof stringrandomNumber)
+
+
+
+// OPERATIONS
+
+
+let value = 5
+let negValue = -value
+console.log(negValue)
+
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+
+let str1="Hello"
+let str2=" Alyvia!"
+let str3= str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+num1=num2=num3=4+3
+
+let gameCounter= 50
+++gameCounter
+gameCounter++
+console.log(gameCounter)
