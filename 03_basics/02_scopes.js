64c64e885ec24e72d8b45ca9cebdb50bccdd53ae
Alyvia Biswas <alyvia02510@gmail.com>
2024-12-16 16:37:05 +0000

    More on Scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,3 +9,41 @@ console.log(a);
 // console.log(b);
 // console.log(c);
 
+function one(){
+    const username="alyvia"
+
+    function two(){
+        const website="YouTube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    two()
+}
+one()
+//(child function can access parent function but parent function cannot access child function)
+
+if(true){
+    const username="alyvia"
+    if(username==="alyvia"){
+        const website = " YouTube"
+        console.log(username+website)
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+
+
+
+//console.log(addone(7)) is possible
+function addone(num){
+    return num+1
+}
+console.log(addone(7))
+
+//console.log(addTwo(7)) is not possible
+const addTwo = function(num){
+    return num+2
+}
+console.log(addTwo(7))
