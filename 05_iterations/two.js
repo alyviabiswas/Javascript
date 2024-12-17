9ad6d15becfa2d83c9986c344dbf983f2ac2d9ea
Alyvia Biswas <alyvia02510@gmail.com>
2024-12-17 13:00:57 +0000

    While and do-while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+// While Loop
+let index=1
+while (index<=5) {
+    console.log(`The value of index is: ${index}`)
+    index=index+2;
+}
+
+let myArray=["flash","BatMan","SpiderMan","Wolverine"]
+let arr=0
+while(arr<=myArray.length){
+    console.log(`The Hero is: ${myArray[arr]}`)
+        arr++;
+    }
+
+// Do-while Loop
+let score=10
+do {
+    console.log("Disqualified!")
+    score++;
+} while (score<5);    
